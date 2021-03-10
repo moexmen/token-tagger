eced76decbdfb07254594141099a2f626c6f93c5
Naomi Leow <naomileow@estl.sg>
2021-03-10 19:18:30 +0800

    Use a single state variable to hold both student and result (#42)

--- app/javascript/components/StudentTaggerContainer.tsx
+++ app/javascript/components/StudentTaggerContainer.tsx
@@ -9,8 +9,10 @@ interface Props {
 
 
 export default (props: Props) => {
-  const [student, setStudent ] = useState(props.student);
-  const [result, setResult ] = useState<Result|undefined>(undefined);
+  const [studentResult, setStudentResult] = useState<{student: Student, result: Result | undefined}>({
+    student: props.student,
+    result: undefined
+  })
   const [csrfToken, setCsrfToken] = useState(props.csrfToken);
 
   const [isFetching, setIsFetching] = useState(false);
@@ -23,7 +25,7 @@ export default (props: Props) => {
     setIsFetching(true);
 
     if (tokenId !== '') {
-      setResult(null);
+      setStudentResult(r => ({...r, result: undefined }));
       
       fetch('/students/tag', {
         method: 'POST',
@@ -36,21 +38,27 @@ export default (props: Props) => {
         .then(data => {
           setIsFetching(false);
 
-          setResult(data.result);
-          setStudent(data.next_student);
+          setStudentResult({
+            student: data.next_student,
+            result: data.result
+          });
           setCsrfToken(data.csrf_token);
         }).catch(e => {
           setIsFetching(false);
-          
-          setResult({
-            success: false,
-            reason: FailureReasons.ApiError,
-            student: student
-          });
+          setStudentResult(r => (
+            {
+              ...r,
+              result: {
+                success: false,
+                reason: FailureReasons.ApiError,
+                student: r.student
+              }
+            }
+          ));
         });
     }
   }
 
-  return <StudentTagger isFetching={isFetching} result={result} student={student} assignToken={assignToken} />
+  return <StudentTagger isFetching={isFetching} result={studentResult.result} student={studentResult.student} assignToken={assignToken} />
 }
 
